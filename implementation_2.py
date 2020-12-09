f48aa4fd16efb5375baedaa51a9670c11809956d
Theo <theosech@mit.edu>
2020-12-08 17:52:03 -0800

    modified code to run impl_2 independently

--- implementation_2.py
+++ implementation_2.py
@@ -1,6 +1,7 @@
 from itertools import chain
 import numpy as np
 import torch
+import json
 from sklearn.neural_network._base import ACTIVATIONS
 from sklearn.neural_network import MLPClassifier
 
@@ -32,7 +33,7 @@ def train(train_negative_pairs, train_positive_pairs, idx_to_vec):
     train_faces = mlp2_preprocessing(idx_to_vec, train_faces_indices)
 
 
-    mlp = MLPClassifier(hidden_layer_sizes=[512], max_iter=10000)
+    mlp = MLPClassifier(hidden_layer_sizes=[512,512,512], max_iter=10000, random_state=4)
     model = mlp.fit(train_faces, train_faces_labels)
     print(model.loss_)
     return model
@@ -52,26 +53,31 @@ def get_classification_accuracy(mlp, test_negative_pairs, test_positive_pairs, i
 
     return similarity_classifier_accuracy(test_negative_pairs, test_positive_pairs, mlp_idx_to_vec, use_corr)
 
-def main(train_negative_pairs, train_positive_pairs, test_negative_pairs, test_positive_pairs, idx_to_vec):
-    model = train(train_negative_pairs, train_positive_pairs, idx_to_vec)
-    accuracy = get_classification_accuracy(model, test_negative_pairs, test_positive_pairs, idx_to_vec)
-    return accuracy
-
 
 if __name__ == "__main__":
 
-    idx_to_vec = get_idx_to_vec('cornet2')
+    idx_to_vec = get_idx_to_vec('densenet169')
     c_test_offset_values = [0,1,2,3,4]
+    accuracies = {c:[] for c in c_test_offset_values}
+    k_fold = 0
+
     for train_pairs, test_pairs in k_fold_sample(2, [0,1,2,3,4], c_test_offset_values):
         train_negative_pairs, train_positive_pairs = train_pairs[0], train_pairs[1]
-        test_negative_pairs, test_positive_pairs = get_test_pairs(test_pairs[0], test_pairs[1], 1, len(c_test_offset_values))
 
-        train_faces = set(list(chain.from_iterable(train_positive_pairs)) + list(chain.from_iterable(train_negative_pairs)))
-        test_faces = set(list(chain.from_iterable(test_positive_pairs)) + list(chain.from_iterable(test_negative_pairs)))
-        assert len(train_faces.intersection(test_faces)) == 0
+        for c in c_test_offset_values:
+            test_negative_pairs, test_positive_pairs = get_test_pairs(test_pairs[0], test_pairs[1], c, len(c_test_offset_values))
 
-        print("Num Negative Test Pairs: {}".format(len(test_negative_pairs)))
-        print("Num Positive Test Pairs: {}".format(len(test_positive_pairs)))
-        model = train(train_negative_pairs, train_positive_pairs, idx_to_vec)
-        accuracy = get_classification_accuracy(model, test_negative_pairs, test_positive_pairs, idx_to_vec)
-        print("Accuracy: {}".format(accuracy))
+            train_faces = set(list(chain.from_iterable(train_positive_pairs)) + list(chain.from_iterable(train_negative_pairs)))
+            test_faces = set(list(chain.from_iterable(test_positive_pairs)) + list(chain.from_iterable(test_negative_pairs)))
+            assert len(train_faces.intersection(test_faces)) == 0
+
+            print("Num Negative Test Pairs: {}".format(len(test_negative_pairs)))
+            print("Num Positive Test Pairs: {}".format(len(test_positive_pairs)))
+            model = train(train_negative_pairs, train_positive_pairs, idx_to_vec)
+            accuracy = get_classification_accuracy(model, test_negative_pairs, test_positive_pairs, idx_to_vec)
+            print("Accuracy-c={}: {}".format(c, accuracy))
+            accuracies[c].append(accuracy)
+
+    k_fold += 1
+
+    print(accuracies)
